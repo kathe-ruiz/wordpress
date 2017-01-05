eda2e1729793e4da33a4b777e1be6108281be1a3
JulianBonilla7 <julian.felipe.bonilla@correounivalle.edu.co>
2017-01-05 17:34:02 -0500

    WP-25 Responsive footer

--- wp-content/themes/swapps/templates/footer.php
+++ wp-content/themes/swapps/templates/footer.php
@@ -1,25 +1,27 @@
 <footer class="footer navbar-inverse">
   <div class="container-fluid">
-    <?php //dynamic_sidebar('sidebar-footer'); ?>
-    <div class="footer__logo col-sm-2">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="Footer logo">
-    </div>
-    <div class="footer__info col-sm-3">
-      <p>Carrera 100 # 5 - 169,<br>Cali, Colombia</p>
-      <a href="#">info@misitioweb.com</a>
-    </div>
-    <div class="col-sm-6 col-sm-offset-1">
-      <div class="row">
-        <div class="socialmedia footer__socialmedia col-sm-6 col-sm-push-6">
-          <div class="socialmedia__block">
-            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-            <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-            <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-            <li><a href="#"><i class="fa fa-vimeo" aria-hidden="true"></i></a></li>
+    <div class="row">
+      <?php //dynamic_sidebar('sidebar-footer'); ?>
+      <div class="footer__logo col-sm-2 col-lg-1">
+        <img class="footer__logo__img" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="Footer logo">
+      </div>
+      <div class="footer__info col-sm-3 col-lg-2">
+        <p>Carrera 100 # 5 - 169,<br>Cali, Colombia</p>
+        <a href="#">info@misitioweb.com</a>
+      </div>
+      <div class="col-sm-6 col-sm-offset-1 col-lg-offset-3">
+        <div class="row">
+          <div class="socialmedia footer__socialmedia col-sm-8 col-sm-push-4 col-md-6 col-md-push-6">
+            <div class="socialmedia_block nav navbar-nav navbar-right">
+              <li class="socialmedia__item"><a href="#" class="socialmedia__link"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li class="socialmedia__item"><a href="#" class="socialmedia__link"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+              <li class="socialmedia__item"><a href="#" class="socialmedia__link"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+              <li class="socialmedia__item"><a href="#" class="socialmedia__link"><i class="fa fa-vimeo" aria-hidden="true"></i></a></li>
+            </div>
           </div>
-        </div>
-        <div class="footer__copyright col-sm-12 text-right">
-          <p>&copy; 2017 misitioweb. Todos los derechos reservados</p>
+          <div class="footer__copyright col-sm-12 text-right">
+            <p>&copy; 2017 misitioweb. Todos los derechos reservados</p>
+          </div>
         </div>
       </div>
     </div>
