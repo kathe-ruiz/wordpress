9ad4448d397d6b3a2b4f7296d89037ed8ad752c8
Luis Carlos Martinez <karloxmartinez@hotmail.com>
2017-01-05 11:12:20 -0500

    Added slider again

--- wp-content/themes/swapps/template-home.php
+++ wp-content/themes/swapps/template-home.php
@@ -10,6 +10,28 @@
   <?php get_search_form(); ?>
 <?php endif; ?>
 <?php $main_slider = get_sw_slider('Main') ?>
+<!-- begin container -->
+<div class="container">
+  <h2>Buttons</h2>
+  <?php if($main_slider): ?>
+    <?php $slides = get_slides_array($main_slider); ?>
+    <div class="owl-carousel owl-theme">
+      <?php foreach ($slides as $key => $slide): ?>
+        <div class="item">
+            <img src="<?php echo $slide['image']['url'] ?>" alt="<?php echo $slide['image']['alt'] ?>" class="img-fluid">
+            <div class="caption">
+              <h2><?php echo $slide['title'] ?></h2>
+              <p><?php echo $slide['description'] ?></p>
+              <a href="<?php echo $slide['link'] ?>" class="btn btn-primary">
+                <?php echo $slide['call_to_action_text'] ?>
+              </a>
+            </div>
+        </div>
+      <?php endforeach ?>
+    </div>
+  <?php endif; ?>
+</div>
+<!-- end container -->
 
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
--- wp-content/themes/swapps/template-style-guide.php
+++ wp-content/themes/swapps/template-style-guide.php
@@ -7,30 +7,33 @@
 
 <!-- begin container -->
 <div class="container">
-  <div class="row">
-    <div class="col-md-12">
-      <h2>Buttons</h2>
-      <hr>
-      <div class="row">
-        <div class="col-md-6">
-          <a href="#" class="btn btn-default">Default</a>
-          <a href="#" class="btn btn-info">Info</a>
-          <a href="#" class="btn btn-primary">Primary</a>
-          <a href="#" class="btn btn-success">Success</a>
-          <a href="#" class="btn btn-warning">Warning</a>
-          <a href="#" class="btn btn-danger">Danger</a>
+  <h2>Buttons</h2>
+  <?php if($main_slider): ?>
+    <?php $slides = get_slides_array($main_slider); ?>
+    <div class="owl-carousel owl-theme">
+      <?php foreach ($slides as $key => $slide): ?>
+        <div class="item">
+            <img src="<?php echo $slide['image']['url'] ?>" alt="<?php echo $slide['image']['alt'] ?>" class="img-fluid">
+            <div class="caption">
+              <h2><?php echo $slide['title'] ?></h2>
+              <p><?php echo $slide['description'] ?></p>
+              <a href="<?php echo $slide['link'] ?>" class="btn btn-primary">
+                <?php echo $slide['call_to_action_text'] ?>
+              </a>
+            </div>
         </div>
-        <div class="col-md-6">
-          <a href="#" class="btn btn-default-outline">Default</a>
-          <a href="#" class="btn btn-info-outline">Info</a>
-          <a href="#" class="btn btn-primary-outline">Primary</a>
-          <a href="#" class="btn btn-success-outline">Success</a>
-          <a href="#" class="btn btn-warning-outline">Warning</a>
-          <a href="#" class="btn btn-danger-outline">Danger</a>
-        </div>
-      </div>
-
+      <?php endforeach ?>
     </div>
+  <?php endif; ?>
+  <div class="text-center">
+    <a href="#" class="btn btn-default">Default</a>
+    <a href="#" class="btn btn-info">Info</a>
+    <a href="#" class="btn btn-primary">Primary</a>
+    <a href="#" class="btn btn-success">Success</a>
+    <a href="#" class="btn btn-warning">Warning</a>
+    <a href="#" class="btn btn-danger">Danger</a>
+    <a href="#" class="btn btn-primary-outline">Outline</a>
+    <a href="#" class="btn btn-theme">Theme</a>
   </div>
 </div>
 <!-- end container -->
