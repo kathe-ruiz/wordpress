a5bdc0da5c464f6cbe7dad770e9d9eed77dc2486
Luis Carlos Martinez <karloxmartinez@hotmail.com>
2017-01-05 11:08:01 -0500

    Added Buttons styles

--- wp-content/themes/swapps/template-style-guide.php
+++ wp-content/themes/swapps/template-style-guide.php
@@ -5,3 +5,32 @@
 ?>
 <?php get_template_part('templates/page', 'header'); ?>
 
+<!-- begin container -->
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <h2>Buttons</h2>
+      <hr>
+      <div class="row">
+        <div class="col-md-6">
+          <a href="#" class="btn btn-default">Default</a>
+          <a href="#" class="btn btn-info">Info</a>
+          <a href="#" class="btn btn-primary">Primary</a>
+          <a href="#" class="btn btn-success">Success</a>
+          <a href="#" class="btn btn-warning">Warning</a>
+          <a href="#" class="btn btn-danger">Danger</a>
+        </div>
+        <div class="col-md-6">
+          <a href="#" class="btn btn-default-outline">Default</a>
+          <a href="#" class="btn btn-info-outline">Info</a>
+          <a href="#" class="btn btn-primary-outline">Primary</a>
+          <a href="#" class="btn btn-success-outline">Success</a>
+          <a href="#" class="btn btn-warning-outline">Warning</a>
+          <a href="#" class="btn btn-danger-outline">Danger</a>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+<!-- end container -->
