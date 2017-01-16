545e559a3612f578b2ab111a6a149900fefac479
Luis Carlos Martinez <karloxmartinez@hotmail.com>
2017-01-16 16:39:34 -0500

    WP-56 Initial implemtentation of 'quienes somos' page.

---
+++ wp-content/themes/swapps/template-about-us.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Template Name: About Us Template
+ */
+?>
+<section class="contact-page">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 information">
+        <div class="heading-underline">
+          <h3 class="heading-underline__title">Nuestra Compañia</h3>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium quae eligendi in odit voluptatum nisi autem labore debitis sequi saepe, quidem, dolorum dolore, placeat ratione obcaecati error tempore eum temporibus?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque laudantium enim vero atque accusantium non ut temporibus soluta, repudiandae provident, ipsam consequuntur ex eos veniam, accusamus reiciendis dolor odit. Quia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis consequatur, velit, rerum modi qui repellendus cupiditate deleniti obcaecati officia voluptates quaerat autem error minima eum nihil laudantium! Quas quo, cumque!
+        </p>
+      </div>
+      <div class="col-sm-6">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/about-us.jpg" alt="" class="img-responsive">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 information">
+        <div class="heading-underline">
+          <h3 class="heading-underline__title">Nuestra Misión</h3>
+        </div>
+      </div>
+      <div class="col-md-12">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio dolores cumque praesentium iure eaque voluptatibus eum minus dignissimos ipsam dolorem, officia, incidunt, explicabo hic et excepturi beatae architecto sed saepe.</p>
+        <p>Odit eveniet numquam voluptatum error dolorem pariatur aut mollitia tempora necessitatibus ab expedita ducimus earum soluta nulla nam ullam maiores magni, perspiciatis nemo laudantium enim sapiente quibusdam libero quisquam, adipisci?</p>
+        <p>Repellendus iste tempora esse fugiat ipsa sunt ea perspiciatis, nisi sed, deleniti assumenda. Odio fugit, corrupti sequi odit eius, excepturi quis ipsam non esse molestiae magni at velit illo voluptate.</p>
+        <p>Tempora inventore, tempore officiis excepturi qui quis amet debitis non enim? Enim accusamus provident quia reiciendis deserunt dolores dignissimos sapiente assumenda quisquam esse, vero non cum nesciunt nisi eius deleniti.</p>
+        <p>Quaerat quos aliquid ducimus perspiciatis. Sed quae culpa repellendus, harum quis. Consequuntur aut, tenetur accusamus eos, fugiat sapiente quod facilis debitis a dignissimos adipisci, totam ipsum ab corrupti. Facilis, corporis.</p>
+        <p>Quas autem tempore dignissimos natus distinctio repellat velit sint nesciunt sit voluptatum eligendi doloremque provident odio placeat, officiis omnis ullam assumenda facilis asperiores. Beatae at molestiae, maiores dignissimos architecto praesentium.</p>
+      </div>
+    </div>
+  </div>
+</section>
