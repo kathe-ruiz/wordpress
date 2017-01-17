fa12a08857e17e76743a2029512dfee2c0c4df25
JulianBonilla7 <julian.felipe.bonilla@correounivalle.edu.co>
2017-01-17 08:04:12 -0500

    Add padding top and define columns for internal pages

--- wp-content/themes/swapps/base.php
+++ wp-content/themes/swapps/base.php
@@ -8,7 +8,8 @@ use Roots\Sage\Wrapper;
 <!doctype html>
 <html <?php language_attributes(); ?>>
   <?php get_template_part('templates/head'); ?>
-  <body <?php body_class(); ?>>
+  <?php $body_header_class = (!is_front_page()) ? 'has-header' : '' ; ?>
+  <body <?php body_class($body_header_class); ?>>
     <!--[if IE]>
       <div class="alert alert-warning">
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
@@ -18,13 +19,13 @@ use Roots\Sage\Wrapper;
       do_action('get_header');
       get_template_part('templates/header');
     ?>
-    <div class="wrap container-fluid" role="document">
+    <div class="wrap container<?php if (is_front_page()): echo '-fluid'; endif; ?>" role="document">
       <div class="content row">
-        <main class="main">
+        <main class="main <?php if (!is_front_page()): echo 'col-sm-8'; endif; ?>">
           <?php include Wrapper\template_path(); ?>
         </main><!-- /.main -->
         <?php if (Setup\display_sidebar()) : ?>
-          <aside class="sidebar">
+          <aside class="sidebar <?php if (!is_front_page()): echo 'col-sm-4'; endif; ?>">
             <?php include Wrapper\sidebar_path(); ?>
           </aside><!-- /.sidebar -->
         <?php endif; ?>
