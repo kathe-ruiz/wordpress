9372bcc3675aeb720b38b81eea321713032b9743
JulianBonilla7 <julian.felipe.bonilla@correounivalle.edu.co>
2017-01-17 16:37:06 -0500

    WP-59 Add breadcrumb function

--- wp-content/themes/swapps/templates/page-header.php
+++ wp-content/themes/swapps/templates/page-header.php
@@ -4,8 +4,9 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-        <!-- <h4 class="breadcrumb__item">Inicio <small>></small> Blog</h4> -->
-        <h1 class="breadcrumb__item breadcrumb__item--active"><?= Titles\title(); ?></h1>
+        <!-- <h4 class="breadcrumb__item">Inicio <small>></small> Blog</h4>
+        <h1 class="breadcrumb__item breadcrumb__item--active"><?//= Titles\title(); ?></h1> -->
+          <?php custom_breadcrumbs(); ?>
       </div>
     </div>
   </div>
