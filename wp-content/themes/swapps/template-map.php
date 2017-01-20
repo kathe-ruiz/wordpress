e521368ba3ba7887f6ba5af2f9dbb6fbfe5328d5
Luis Carlos Martinez <karloxmartinez@hotmail.com>
2017-01-20 17:14:05 -0500

    Added container into template-map

--- wp-content/themes/swapps/template-map.php
+++ wp-content/themes/swapps/template-map.php
@@ -3,7 +3,9 @@
  * Template Name: Map Template
  */
 ?>
+<div class="container">
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
+</div>
 <?php include 'templates/includes/component-map.php' ?>
