1284edd574ac3c99ce3bf4f69f809ab09fc68eb4
jose ariza <jariza@swapps.co>
2017-01-25 07:29:35 -0500

    WP-97 added text center to image grid

--- wp-content/themes/swapps/layouts/layout_images_grid.php
+++ wp-content/themes/swapps/layouts/layout_images_grid.php
@@ -12,13 +12,13 @@ if($total < 3){
 	<div class="row">
 	<?php foreach ($row_item['grid_elements'] as $grid_element): ?>
 		<?php if($row_item['grid_type']=='images'): ?>
-			<div class="col-md-<?php echo $grid_size ?> col-md-offset-<?php echo $offset_size ?>">
+			<div class="col-md-<?php echo $grid_size ?> col-md-offset-<?php echo $offset_size ?> text-center">
 				<img src="<?php echo $grid_element['image']['url'] ?>">
 				<h4 class="icons__title text-uppercase"><?php echo $grid_element['title'] ?></h4>
 				<p class="icons__text"><?php echo $grid_element['description'] ?></p>
 			</div>
 		<?php elseif($row_item['grid_type']=='icons'): ?>
-			<div class="col-md-<?php echo $grid_size ?> col-md-offset-<?php echo $offset_size ?>">
+			<div class="col-md-<?php echo $grid_size ?> col-md-offset-<?php echo $offset_size ?> text-center">
 				<?php echo $grid_element['font_icon'] ?>
 				<h4 class="icons__title text-uppercase"><?php echo $grid_element['title'] ?></h4>
 				<p class="icons__text"><?php echo $grid_element['description'] ?></p>
