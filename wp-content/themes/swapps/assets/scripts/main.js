55f024d589e75c967cbdfcf88860622f02455c10
brayan <bralavar@gmail.com>
2017-01-26 15:59:57 -0500

    WP-101 Make breakpoint setup dynamically.

--- wp-content/themes/swapps/assets/scripts/main.js
+++ wp-content/themes/swapps/assets/scripts/main.js
@@ -71,6 +71,15 @@
           'opacity': true
 
         });
+        function autocollapse() {
+          var navbar = $('#autocollapse');
+          navbar.removeClass('collapsed'); // set standart view
+          if(navbar.innerHeight() > 76) // check if we've got 2 lines
+            navbar.addClass('collapsed'); // force collapse mode
+        }
+
+        $(document).on('ready', autocollapse);
+        $(window).on('resize', autocollapse);
       },
       finalize: function() {
         // JavaScript to be fired on all pages, after page specific JS is fired
