418c86beb2162347d1cdfaf3c5b6b0a445b24ad4
brayan <bralavar@gmail.com>
2017-01-30 13:13:59 -0500

    WP-101 Make breakpoint setup dynamically

--- wp-content/themes/swapps/assets/scripts/main.js
+++ wp-content/themes/swapps/assets/scripts/main.js
@@ -71,7 +71,7 @@
           'opacity': true
 
         });
-        function autocollapse() {
+        function autocollapse(){
           var navbar = $('#autocollapse');
           navbar.removeClass('collapsed'); // set standart view
           if(navbar.innerHeight() > 76) // check if we've got 2 lines
