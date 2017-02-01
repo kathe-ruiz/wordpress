5c24ee1fdc0794819111ab73f3647cb638741994
Luis Carlos Martinez <karloxmartinez@hotmail.com>
2017-02-01 15:22:37 -0500

    Hide all html when socialmedia networks is not configured

--- wp-content/themes/swapps/templates/includes/socialmedia.php
+++ wp-content/themes/swapps/templates/includes/socialmedia.php
@@ -1,34 +1,35 @@
+<?php $accounts = get_social_accounts(); ?>
+<?php if (isset($accounts)): ?>
 <div class="socialmedia__block nav navbar-nav navbar-right">
-  <?php $accounts = get_social_accounts(); ?>
-  <?php if ($accounts['facebook']): ?>
+  <?php if (isset($accounts['facebook'])): ?>
   <li class="socialmedia__item">
     <a href="<?php echo $accounts['facebook'] ?>" class="socialmedia__link" target="_blank">
       <i class="fa fa-facebook" aria-hidden="true"></i>
     </a>
   </li>
   <?php endif ?>
-  <?php if ($accounts['twitter']): ?>
+  <?php if (isset($accounts['twitter'])): ?>
   <li class="socialmedia__item">
     <a href="<?php echo $accounts['twitter'] ?>" class="socialmedia__link" target="_blank">
       <i class="fa fa-twitter" aria-hidden="true"></i>
     </a>
   </li>
   <?php endif ?>
-  <?php if ($accounts['instagram']): ?>
+  <?php if (isset($accounts['instagram'])): ?>
   <li class="socialmedia__item">
     <a href="<?php echo $accounts['instagram'] ?>" class="socialmedia__link" target="_blank">
       <i class="fa fa-instagram" aria-hidden="true"></i>
     </a>
   </li>
   <?php endif ?>
-  <?php if ($accounts['vimeo']): ?>
+  <?php if (isset($accounts['vimeo'])): ?>
   <li class="socialmedia__item">
     <a href="<?php echo $accounts['vimeo'] ?>" class="socialmedia__link" target="_blank">
       <i class="fa fa-vimeo" aria-hidden="true"></i>
     </a>
   </li>
   <?php endif ?>
-  <?php if ($accounts['linkedin']): ?>
+  <?php if (isset($accounts['linkedin'])): ?>
   <li class="socialmedia__item">
     <a href="<?php echo $accounts['linkedin'] ?>" class="socialmedia__link" target="_blank">
       <i class="fa fa-linkedin" aria-hidden="true"></i>
@@ -36,3 +37,4 @@
   </li>
   <?php endif ?>
 </div>
+<?php endif ?>
