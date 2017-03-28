c31d8fd26207494d1f19f2d4ae708ca1a9928f57
brayan <bralavar@gmail.com>
2017-03-28 15:09:51 -0500

    WP-244 "Valoraciones" tab in product detail not working

--- wp-content/themes/swapps/assets/scripts/main.js
+++ wp-content/themes/swapps/assets/scripts/main.js
@@ -117,7 +117,7 @@
         $(window).on('resize', autocollapse);
 
       $(function() {
-        $('a[href*="#"]:not([href="#"])').click(function() {
+        $('.navbar a[href*="#"]:not([href="#"])').click(function() {
           if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
           var target = $(this.hash);
           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
