4a43fbe4bb45da6757190ace1340107c94de9ac8
Luis Carlos Martinez <karloxmartinez@hotmail.com>
2017-04-03 13:14:39 -0500

    Reduce width in breadcrumb text

--- wp-content/themes/swapps/templates/page-header.php
+++ wp-content/themes/swapps/templates/page-header.php
@@ -18,7 +18,7 @@
       <?php if(isset($background)): ?> style="background-image: url(<?php echo $background ?>); background-size: cover;"<?php endif; ?>>
       <div class="container">
         <div class="row">
-          <div class="col-xs-12">
+          <div class="col-xs-10">
             <!-- <h4 class="breadcrumb__item">Inicio <small>></small> Blog</h4>
             <h1 class="breadcrumb__item breadcrumb__item--active"><?//= Titles\title(); ?></h1> -->
               <?php swapps_breadcrumbs(); ?>
