d9e1bae36dc2e82188ef38679033a6617790ce97
brayan <bralavar@gmail.com>
2017-05-11 15:52:04 -0500

    WP-382 Fix filter not permit click in forms

--- wp-content/themes/swapps/layouts/layout_subscribe.php
+++ wp-content/themes/swapps/layouts/layout_subscribe.php
@@ -1,5 +1,5 @@
 <?php if ( function_exists( 'mc4wp_show_form' ) && isset($row_item['sign_up']) ): ?>
-<div class="container">
+<div class="container subscribe">
   <div class="row text-center">
     <?php mc4wp_show_form(); ?>
   </div>
