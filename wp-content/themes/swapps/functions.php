8fbabff4b53d22c84e73c367b223cc627a8e7dd9
brayan <bralavar@gmail.com>
2017-06-13 13:13:11 -0500

    WP-465 Alerts should not be shown

--- wp-content/themes/swapps/functions.php
+++ wp-content/themes/swapps/functions.php
@@ -459,3 +459,9 @@ function display_internal_sidebar() {
 } 
 add_theme_support( 'post-thumbnails' );
 add_image_size( 'gallery-image', 320, 200, true ); // Hard Crop Mode
+function hide_msg__admins(){
+ if (current_user_can( 'manage_options' )) { // non-admin users
+    echo '<style>.update-nag, .updated , .notice , .error{ display: none !important; }</style>';
+  }
+}
+add_action( 'admin_head', 'hide_msg__admins');
