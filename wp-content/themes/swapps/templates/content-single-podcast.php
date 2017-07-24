944e1f9d92e6a163b446770ef69d08f9634b0b3c
Julián Bonilla <jfbonillago@gmail.com>
2017-07-24 14:23:18 -0500

    WLC-66 Frontend adjustments for podcast detail

--- wp-content/themes/swapps/templates/content-single-podcast.php
+++ wp-content/themes/swapps/templates/content-single-podcast.php
@@ -1,43 +1,43 @@
 
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part( 'templates/podcast', 'head' ) ?>
+  <section class="podcast-detail">
+    <?php get_template_part( 'templates/podcast', 'head' ) ?>
+  </section>
 <?php endwhile; ?>
-<div class="container">
-  <div class="row">
-    <div class="col-md-12">
-      <h2 class="text-center">More from</h2>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-sm-10 col-sm-offset-1">
-      
-      <?php
-      $posts_array = get_posts(
-          array(
-              'posts_per_page' => -1,
-              'post_type' => 'podcast',
-              // 'tax_query' => array(
-              //     array(
-              //         'taxonomy' => 'fabric_building_types',
-              //         'field' => 'term_id',
-              //         'terms' => $cat->term_id,
-              //     )
-              // )
-          )
-      );
-      ?>
-
-    <?php foreach ($posts_array as $key => $podcast): ?>
-      <?php include(locate_template( 'templates/podcast-item.php' )) ?>
-    <?php endforeach ?>
-      <div class="col-md-4">
-        <?php echo $key; ?>
-        <?php print_r($podcast); ?>
+<section class="related-podcasts">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h2 class="h3 list-title text-center">More from <strong>category</strong></h2>
       </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-10 col-sm-offset-1">
+        
+        <?php
+        $posts_array = get_posts(
+            array(
+                'posts_per_page' => -1,
+                'post_type' => 'podcast',
+                // 'tax_query' => array(
+                //     array(
+                //         'taxonomy' => 'fabric_building_types',
+                //         'field' => 'term_id',
+                //         'terms' => $cat->term_id,
+                //     )
+                // )
+            )
+        );
+        ?>
 
+      <?php foreach ($posts_array as $key => $podcast): ?>
+        <?php include(locate_template( 'templates/podcast-item.php' )) ?>
+      <?php endforeach ?>
+
+      </div>
     </div>
   </div>
-</div>
+</section>
 <div class="container">
   <div class="row">
     <div class="col-md-12">
