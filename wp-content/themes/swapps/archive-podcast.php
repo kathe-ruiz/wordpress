e022720b8c2e505830298fb35023a16eef846bea
Luis Carlos Martinez <karloxmartinez@hotmail.com>
2017-07-25 16:40:09 -0500

    Hidden serach form WLC-65

--- wp-content/themes/swapps/archive-podcast.php
+++ wp-content/themes/swapps/archive-podcast.php
@@ -11,21 +11,21 @@
   <?php $row = 1; ?>
 <?php endwhile; ?>
 
-<div class='card-wrapper row _pdt50'>
-  <div class='col-sm-8 col-sm-offset-2'>
-    <div class='sermon-search search-bar row'>
+<?php '<div class="card-wrapper row _pdt50">
+  <div class="col-sm-8 col-sm-offset-2">
+    <div class="sermon-search search-bar row">
       <form accept-charset="UTF-8" action="/podcast" class="inline-form" method="get">
-        <div class='form-group col-sm-9'>
+        <div class="form-group col-sm-9">
           <input class="form-control" id="s" name="s" placeholder="Search for a podcast" type="text" />
         </div>
-        <div class='form-group col-sm-3'>
+        <div class="form-group col-sm-3">
           <button class="btn btn-podcast btn-block" name="button" type="submit">Search</button>
         </div>
       </form>
     </div>
   </div>
-</div>
-
+</div>'
+ ?>
 <div class="col-md-10 col-md-offset-1 podcast-list">
   <?php while (have_posts()) : the_post(); ?>
     <?php if ($row != 0): ?>
