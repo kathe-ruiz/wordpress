f0a180398fdcf8ce5412dab28db5d705138d73cc
Luis Carlos Martinez <karloxmartinez@hotmail.com>
2017-08-08 10:44:57 -0500

    Added URL to image to open podcast WLC-75

--- wp-content/themes/swapps/templates/podcast-head.php
+++ wp-content/themes/swapps/templates/podcast-head.php
@@ -17,12 +17,13 @@
       <?php endif ?>
       <div class="card-wrapper">
         <div class="card">
-          <?php if ( has_post_thumbnail() ) : ?>
-            <?php the_post_thumbnail('medium_large', array( 'class' => "img-responsive center-block content-single__image full-width")) ?>
-          <?php else: ?>
-
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/pdocast-default-image.png" alt="Podcast image" class="img-responsive center-block content-single__image full-width">
-          <?php endif; ?>
+          <a href="<?php echo get_permalink() ?>">
+            <?php if ( has_post_thumbnail() ) : ?>
+              <?php the_post_thumbnail('medium_large', array( 'class' => "img-responsive center-block content-single__image full-width")) ?>
+            <?php else: ?>
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/pdocast-default-image.png" alt="Podcast image" class="img-responsive center-block content-single__image full-width">
+            <?php endif; ?>
+          </a>
             <div class="entry-content content-single__text">
               <?php the_content(); ?>
             </div>
