379466eaf0abda90f72207543370ce0c0d9e01d0
brayan <bralavar@gmail.com>
2017-08-24 09:00:08 -0500

    WP-499 Breadcrumb adjustments for products

--- wp-content/themes/swapps/breadcrumb.php
+++ wp-content/themes/swapps/breadcrumb.php
@@ -64,12 +64,25 @@ if ( ! function_exists( 'swapps_breadcrumbs' ) ) {
       $shop_page_id = wc_get_page_id( 'shop' );
       $title['title'] = get_the_title( $shop_page_id );
       $permalink['link'] = get_permalink( $shop_page_id );
+      $term = wp_get_post_terms( $post->ID, 'product_cat' );
       $primary_cat_id=get_post_meta($post->ID,'_yoast_wpseo_primary_product_cat',true);
       $html .= '<li class="breadcrumb__item item-' . $shop_page_id . '"><a class="breadcrumb__bread bread-link bread-item-' . $shop_page_id . '" href="' . $permalink['link'] . '" title="' . $title['title'] . '">' . $title['title'] . '</a></li>';
       $html .= $separator;
       if ($primary_cat_id) {
         $product_cat = get_term($primary_cat_id, 'product_cat');
         $link = get_term_link($product_cat->term_id);
+        $parent = $product_cat->parent;
+        if ($parent) {
+          $parent_det = get_term($parent, 'product_cat');
+          $link_parent = get_term_link($parent_det->term_id);
+          $html .= '<li class="breadcrumb__item item-parent item-cat"><a href="'. $link_parent .'">'.$parent_det->name.'</a></li>';
+          $html .= $separator;
+        }
+        /*$children = get_terms( $product_cat->taxonomy, array(
+        'parent'    => $product_cat->term_id,
+        'hide_empty' => false,
+        'childless' => true
+        ));*/
         $html .= '<li class="breadcrumb__item item-cat"><a href="'. $link .'">'.$product_cat->name.'</a></li>';
         $html .= $separator;
       }
