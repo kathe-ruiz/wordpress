74a0f91ea80aad26b3d318798c5c16689764cf06
Katherine Ruiz <kruiz@swapps.co>
2018-10-10 14:34:06 -0500

    Plugin Updated: accelerated-mobile-pages	0.9.97.17	0.9.97.19

--- wp-content/plugins/accelerated-mobile-pages/accelerated-moblie-pages.php
+++ wp-content/plugins/accelerated-mobile-pages/accelerated-moblie-pages.php
@@ -3,7 +3,7 @@
 Plugin Name: Accelerated Mobile Pages
 Plugin URI: https://wordpress.org/plugins/accelerated-mobile-pages/
 Description: AMP for WP - Accelerated Mobile Pages for WordPress
-Version: 0.9.97.17
+Version: 0.9.97.19
 Author: Ahmed Kaludi, Mohammed Kaludi
 Author URI: https://ampforwp.com/
 Donate link: https://www.paypal.me/Kaludi/25
@@ -20,7 +20,7 @@ define('AMPFORWP_PLUGIN_DIR_URI', plugin_dir_url(__FILE__));
 define('AMPFORWP_DISQUS_URL',plugin_dir_url(__FILE__).'includes/disqus.html');
 define('AMPFORWP_IMAGE_DIR',plugin_dir_url(__FILE__).'images');
 define('AMPFORWP_MAIN_PLUGIN_DIR', plugin_dir_path( __DIR__ ) );
-define('AMPFORWP_VERSION','0.9.97.17');
+define('AMPFORWP_VERSION','0.9.97.19');
 // any changes to AMP_QUERY_VAR should be refelected here
 function ampforwp_generate_endpoint(){
     $ampforwp_slug = '';
@@ -733,6 +733,9 @@ require_once( AMP_FRAMEWORK_COMOPNENT_DIR_PATH . '/components-core.php' );
 require_once(  AMPFORWP_PLUGIN_DIR. 'pagebuilder/amp-page-builder.php' );
 require_once(  AMPFORWP_PLUGIN_DIR. 'base_remover/base_remover.php' );
 require_once(  AMPFORWP_PLUGIN_DIR. 'includes/thirdparty-compatibility.php' );
+if(is_admin()){
+require_once(  AMPFORWP_PLUGIN_DIR. '/includes/modules-upgrade.php');
+}
 require ( AMPFORWP_PLUGIN_DIR.'/install/index.php' );
 
 /**
