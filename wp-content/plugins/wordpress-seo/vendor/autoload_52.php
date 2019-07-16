951c4b9a69a0622c618dc3e9f543d6e903d8449e
Katherine Ruiz <kruiz@swapps.co>
2019-07-16 09:01:11 -0500

    Plugin Updated: wordpress-seo	11.4	11.6

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit3df97fd6b08f820fdf0fa0d030844312::getLoader();
+return ComposerAutoloaderInit3db7885aacab90afa91fad77f50b6590::getLoader();
